471fc4481b7ca8c08cafc21266393fe17eafa929
Marcelo Jose Batista Fajardo <chelobat16411@gmail.com>
2022-07-11 18:51:26 -0300

    fix:validation email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,23 @@
-import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import TrackVisibility from "react-on-screen";
-import contactImg from "../assets/images/contact-img.svg";
-import "animate.css";
+import {useState, useRef} from 'react';
+import {Col, Container, Row} from 'react-bootstrap';
+import TrackVisibility from 'react-on-screen';
+import contactImg from '../assets/images/contact-img.svg';
+import 'animate.css';
+import emailjs from '@emailjs/browser';
+
 export const Contact = () => {
   const formInitialDetails = {
-    firstName: "",
-    lastName: "",
-    email: "",
-    phone: "",
-    message: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: '',
   };
   const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText, setButtonText] = useState("Send");
+  const [buttonText, setButtonText] = useState('Send');
   const [status, setStatus] = useState({});
+
+  const form = useRef();
 
   const onFormUpdate = (category, value) => {
     setFormDetails({
@@ -22,27 +26,36 @@ export const Contact = () => {
     });
   };
 
+  const validateFormFields = ({firstName, lastName, email, phone, message}) => {
+    if (!firstName || !lastName || !email || !phone || !message) {
+      setStatus({success: false, message: 'The fields mustn\'t be empty.'})
+      return false
+    }
+    return true
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "Application/json;charset=utf-8",
-      },
-      body: JSON.stringify(formDetails),
-    });
-    setButtonText("Send");
-    let result = await response.json();
-    console.log("CODE FROM SERVER", result);
-    setFormDetails(formInitialDetails);
-    if (result.code === 200) {
-      setStatus({ success: true, message: "Message sent successfully" });
-    } else {
+
+    if (!validateFormFields(formDetails)) return null
+    setButtonText('Sending...');
+    try {
+      const result = await emailjs.sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_USER_ID,
+      );
+      if (result.status === 200)
+        setStatus({success: true, message: 'Message sent successfully'});
+    } catch (error) {
       setStatus({
         success: false,
-        message: "Something went wrong, please try again later.",
+        message: 'Something went wrong, please try again later.',
       });
+    } finally {
+      setButtonText('Send');
+      setFormDetails(formInitialDetails);
     }
   };
 
@@ -52,10 +65,10 @@ export const Contact = () => {
         <Row className="align-items-center">
           <Col md={6}>
             <TrackVisibility>
-              {({ isVisible }) => (
+              {({isVisible}) => (
                 <div
                   className={
-                    isVisible ? "animate__animated animate__bounceInDown" : ""
+                    isVisible ? 'animate__animated animate__bounceInDown' : ''
                   }
                 >
                   <img src={contactImg} alt="Contact me" />
@@ -65,60 +78,67 @@ export const Contact = () => {
           </Col>
           <Col md={6}>
             <h2>Get In Touch</h2>
-            <form onSubmit={handleSubmit}>
+            <form ref={form} onSubmit={handleSubmit}>
               <Row>
                 <Col sm={6} className="px-1">
                   <input
+                    name="firstName"
                     type="text"
                     value={formDetails.firstName}
                     placeholder="First Name"
-                    onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                    onChange={(e) => onFormUpdate('firstName', e.target.value)}
                   />
                 </Col>
                 <Col sm={6} className="px-1">
                   <input
+                    name="lastName"
                     type="text"
                     value={formDetails.lastName}
                     placeholder="Last Name"
-                    onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                    onChange={(e) => onFormUpdate('lastName', e.target.value)}
                   />
                 </Col>
                 <Col sm={6} className="px-1">
                   <input
+                    name="email"
                     type="email"
                     value={formDetails.email}
                     placeholder="Email Address"
-                    onChange={(e) => onFormUpdate("email", e.target.value)}
+                    onChange={(e) => onFormUpdate('email', e.target.value)}
                   />
                 </Col>
                 <Col sm={6} className="px-1">
                   <input
+                    name="phone"
                     type="tel"
                     value={formDetails.phone}
                     placeholder="Phone No."
-                    onChange={(e) => onFormUpdate("phone", e.target.value)}
+                    onChange={(e) => onFormUpdate('phone', e.target.value)}
                   />
                 </Col>
                 <Col>
                   <textarea
+                    name="message"
                     row="6"
                     value={formDetails.message}
                     placeholder="Message"
-                    onChange={(e) => onFormUpdate("message", e.target.value)}
+                    onChange={(e) => onFormUpdate('message', e.target.value)}
                   ></textarea>
-                  <button type="submit">
+                  <button style={{borderRadius: '10px'}} type="submit">
                     <span>{buttonText}</span>
                   </button>
                 </Col>
                 {status.message && (
                   <Col>
-                    <p
-                      className={
-                        status.success === false ? "danger" : "success"
-                      }
-                    >
-                      <h3>{status.message}</h3>{" "}
-                    </p>
+                    <h3>
+                      <p
+                        className={
+                          status.success === false ? 'danger' : 'success'
+                        }
+                      >
+                        {status.message}
+                      </p>
+                    </h3>
                   </Col>
                 )}
               </Row>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,32 @@
-import Carousel from "react-multi-carousel";
-import { Container, Row, Col } from "react-bootstrap";
-import "react-multi-carousel/lib/styles.css";
-import meter1 from "../assets/images/meter1.svg";
-import meter2 from "../assets/images/meter2.svg";
-import meter3 from "../assets/images/meter3.svg";
-import colorSharp from "../assets/images/color-sharp.png";
+import Carousel from 'react-multi-carousel';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'react-multi-carousel/lib/styles.css';
+import meter1 from '../assets/images/meter1.svg';
+import meter2 from '../assets/images/meter2.svg';
+import meter3 from '../assets/images/meter3.svg';
+import colorSharp from '../assets/images/color-sharp.png';
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export const Skills = () => {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
-  };
   return (
     <section className="skill" id="skills">
       <Container>
